f3a53d273c3669de2700abbcc6e274f9494e1547
Murilo Baldi <murilo2.baldi@usp.br>
2020-04-28 22:01:34 -0300

    Migrate config to new own repo

---
+++ init.el
@@ -0,0 +1,7 @@
+;; #################### MURILO'S CORPORATION ####################
+
+(package-initialize)
+
+(org-babel-load-file "~/.emacs.d/configuration.org")
+
+;; ##############################################################
